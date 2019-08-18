435d629c068ebf815faa517fc2db60ae7a19f6cb
Dyneteve <sound0020@gmail.com>
2019-08-18 16:51:33 +0300

    CI: Pull laurus blobs

--- get_rom.py
+++ get_rom.py
@@ -1,19 +1,19 @@
 from requests import get
 from datetime import datetime as dt
 import json
-with open('davinci_stable.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/davinci.json").content)
-fw_stable = json.loads(open('davinci_stable.json').read())
-stable_date = dt.strptime(fw_stable[1]["date"], "%Y-%m-%d")
+with open('laurus_stable.json', 'wb') as load:
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/laurus.json").content)
+fw_stable = json.loads(open('laurus_stable.json').read())
+stable_date = dt.strptime(fw_stable[0]["date"], "%Y-%m-%d")
 if stable_date:
     URL="https://bigota.d.miui.com/"
-    version=fw_stable[1]["versions"]["miui"]
+    version=fw_stable[0]["versions"]["miui"]
     with open('/tmp/version','wb') as load:
         load.write(str.encode(version))
     URL+=version
     URL+="/"
-    file=fw_stable[1]["filename"]
-    file=file[11:]
+    file=fw_stable[0]["filename"]
+    file=file[10:]
     URL+=file
     print("Fetching Stable ROM......")
 with open('rom.zip', 'wb') as load:
